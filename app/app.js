cd1b99b7afb4e7a4a2ef1f1641efb68774124c06
Menaka Sirisena <menaka.sirisena@eyepax.com>
2019-08-18 00:56:25 +0530

    add NinjaController

--- app/app.js
+++ app/app.js
@@ -9,4 +9,29 @@ myNinjaApp.run(function(){
 
 });
 
-myNinjaApp.controller
+myNinjaApp.controller('NinjaController', ['$scope',function($scope){
+    $scope.message = "hey y'all";
+
+    $scope.ninjas = [
+        {
+            name: "yoshi",
+            belt: "green",
+            rate: 50
+        },
+        {
+            name: "Crystal",
+            belt: "red",
+            rate: 30
+        },
+        {
+            name: "Ryu",
+            belt: "black",
+            rate: 20
+        },
+        {
+            name: "Shaun",
+            belt: "brown",
+            rate: 45
+        }
+    ];
+}]);
